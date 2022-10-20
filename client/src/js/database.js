33ffa768be85f5429b6cc5da6512e3ef3275c810
SLeBron <sophieclebron@gmail.com>
2022-10-19 23:13:57 -0600

    added methods to put and get content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('updated to database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.pull({id: 1, jate: content});
+  const result = await request;
+  console.log('putDb implemented', result);
+  console.error('putDb not implemented');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('get all data from the database');
+  const jateDb = await openDB('jate', 1);
+  const txt = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('getDb implemented', result);
+  console.error('getDb not implemented');
+}
 
 initdb();

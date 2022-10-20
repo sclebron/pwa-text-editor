ca67d0820e317ffbcefde2df17aa2c270b678f1f
SLeBron <sophieclebron@gmail.com>
2022-10-19 23:34:19 -0600

    fixed typo in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,7 +28,7 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('get all data from the database');
   const jateDb = await openDB('jate', 1);
-  const txt = jateDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
